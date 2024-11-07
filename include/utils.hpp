afd7313a7828452dbc7bd36d6e370fd48893de42
root <root@DESKTOP-QK42V7B>
2024-11-07 22:01:58 +0100

    main

---
+++ include/utils.hpp
@@ -0,0 +1,14 @@
+// Include guard
+#ifndef __utils_hpp__  
+#define __utils_hpp__
+
+
+
+
+
+
+
+
+
+
+#endif  // end of include guard __utils_hpp__
---
+++ testefil_1.cpp
@@ -0,0 +1,10 @@
+// Execute with: 
+// g++ double_ion.cpp src/utils.cpp -I include -larmadillo -o double_ion.exe; ./double_ion.exe
+
+#include <iomanip>
+#include "utils.hpp" // Header file
+
+int main()
+{
+
+}
