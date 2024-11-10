a3d56b1451a4325621e38c27a483c30e249506aa
root <root@DESKTOP-QK42V7B>
2024-11-10 13:55:27 +0100

    changes

--- include/utils.hpp
+++ include/utils.hpp
@@ -2,8 +2,27 @@
 #ifndef __utils_hpp__  
 #define __utils_hpp__
 
+#include <vector>
 
 
+
+class Ising_lattice
+{
+public:
+    unsigned int L; // Length of lattice
+    double T; // Temperature
+    double J; // Coupling constant
+
+    std::vector <std::vector<bool>> Lattice; // The lattice
+    std::vector <double> Bf; // Possible Boltzmann factors
+
+
+    // Contructor
+    Ising_lattice(unsigned int length, double temp, double coup_const);
+};
+
+// Function for implementing periodic boundary conditions
+int pbc(int i, unsigned int L, int add);
 
 
 
--- testefil_1.cpp
+++ testefil_1.cpp
@@ -1,10 +1,12 @@
 // Execute with: 
-// g++ double_ion.cpp src/utils.cpp -I include -larmadillo -o double_ion.exe; ./double_ion.exe
+// g++ testefil_1.cpp src/utils.cpp -I include -o testefil_1.exe; ./testefil_1.exe
 
 #include <iomanip>
+#include <iostream>
 #include "utils.hpp" // Header file
 
 int main()
 {
+    Ising_lattice lattice = Ising_lattice(3, 3, 1);
 
 }
