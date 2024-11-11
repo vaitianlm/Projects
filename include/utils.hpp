0735dba8293193e6788549e07b8aa5efb84c8d9e
root <root@DESKTOP-QK42V7B>
2024-11-11 22:58:22 +0100

    changes

--- include/utils.hpp
+++ include/utils.hpp
@@ -12,17 +12,35 @@ public:
     unsigned int L; // Length of lattice
     double T; // Temperature
     double J; // Coupling constant
+    double E; // Energy
+    int M; // magnetisation
 
-    std::vector <std::vector<bool>> Lattice; // The lattice
+    std::vector <std::vector<int>> Lattice; // The lattice
     std::vector <double> Bf; // Possible Boltzmann factors
 
 
     // Contructor
     Ising_lattice(unsigned int length, double temp, double coup_const);
+
+    // Function for implementing periodic boundary conditions
+    int pbc(int i, int add);
+
+    // Computes total magnetisation of lattice
+    int magnetisation();
+
+    // Computes total energy of lattice
+    double energy_tot();
+
+    // Computes difference in energy after spin flip
+    double energy_diff(int i, int j);
+
+    // Does one MCMC step on spin L_ij
+    void MCMC_step(double r, int i, int j);
+
+    // Does one MCMC cycle
+    void MCMC_cycle();
 };
 
-// Function for implementing periodic boundary conditions
-int pbc(int i, unsigned int L, int add);
 
 
 
--- testefil_1.cpp
+++ testefil_1.cpp
@@ -4,9 +4,12 @@
 #include <iomanip>
 #include <iostream>
 #include "utils.hpp" // Header file
+using namespace std;
 
 int main()
 {
-    Ising_lattice lattice = Ising_lattice(3, 3, 1);
-
+    Ising_lattice lattice = Ising_lattice(2, 3, 1);
+    lattice.Lattice[0][0] = -1;
+    lattice.Lattice[1][1] = -1;
+    cout<< lattice.energy_tot() << endl;
 }
