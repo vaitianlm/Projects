02430e1d9f290133d4e730f166045867d1bd10a8
root <root@DESKTOP-QK42V7B>
2024-12-04 14:43:35 +0100

    ferdig med oppg 2

--- include/utils.hpp
+++ include/utils.hpp
@@ -7,35 +7,33 @@
 class Double_slit
 {
 public:
-    
+    int M;              // Points along the spacial axes
     double dt;          // Stepsize time
     double h;           // Stepsize space
     arma::cx_double r;  // i*dt/2h^2
-    arma::cx_mat A;     // CN scheme matrix A
-    arma::cx_mat B;     // CN scheme matrix B
+    // arma::sp_cx_mat A;     // CN scheme matrix A
+    // arma::sp_cx_mat B;     // CN scheme matrix B
 
     // Contructor
-    Double_slit(double dt, double h, arma::cx_vec a, arma::cx_vec b);
+    Double_slit(int points, double dt, double h, arma::cx_vec a, arma::cx_vec b);
 
     // Translates two indices i,j to single indice k  (O)
     int vec_ind(int i, int j, int M);
 
     // Generates vector containing diagonal elements of matrix A
-    arma::cx_vec a_vec(const arma::cx_mat& V, arma::cx_double r, arma::cx_double dt);
+    arma::cx_vec a_vec(const arma::sp_cx_mat& V, arma::cx_double r, arma::cx_double dt);
 
     // generates vector containing diagonal elements of matrix B
-    arma::cx_vec b_vec(const arma::cx_mat& V, arma::cx_double r, arma::cx_double dt);
+    arma::cx_vec b_vec(const arma::sp_cx_mat& V, arma::cx_double r, arma::cx_double dt);
 
     // Helper function for CN_matA and CN_matB
-    arma::cx_mat tridiag(arma::cx_double r, const arma::cx_vec& di);
+    arma::sp_cx_mat tridiag(arma::cx_double r, const arma::cx_vec& di);
 
-    // Constructs matrix A for CN scheme
-    arma::cx_mat CN_matA(int M, arma::cx_double r, arma::cx_vec a);
+    // Constructs matrices A and B for CN scheme
+    arma::sp_cx_mat CN_matAB( arma::cx_double r, arma::cx_vec d);
 
-    // Constructs matrix B for CN scheme
-    arma::cx_mat CN_matB(int M, arma::cx_double r, arma::cx_vec b);
 };
 
-
+void print_sp_matrix_structure(const arma::sp_cx_mat& A);
 
 #endif  // end of include guard __utils_hpp__
--- src/utils.cpp
+++ src/utils.cpp
@@ -8,8 +8,9 @@ using namespace std::complex_literals;
 using namespace std;
 
 // Contructor
-Double_slit::Double_slit(double timestep, double spacestep, arma::cx_vec a, arma::cx_vec b)
+Double_slit::Double_slit(int points, double timestep, double spacestep, arma::cx_vec a, arma::cx_vec b)
 {
+    M = points;
     dt = timestep;
     h = spacestep;
     r = 1i*dt/(2*pow(h, 2));
@@ -17,66 +18,168 @@ Double_slit::Double_slit(double timestep, double spacestep, arma::cx_vec a, arma
     //B = CN_matB(r, b);
 }
 
-// Translates two indices i,j to a single index k
+// Translates two indices i,j of (M-2 x M-2) matrix to a single index k
 int Double_slit::vec_ind(int i, int j, int M)
 {
     return i + j*(M - 2);
 }
 
 // generates vector containing diagonal elements of matrix A
-arma::cx_vec Double_slit::a_vec(const arma::cx_mat& V, arma::cx_double r, arma::cx_double dt)
+arma::cx_vec Double_slit::a_vec(const arma::sp_cx_mat& V, arma::cx_double r, arma::cx_double dt)
 {
-    arma::cx_vec v = V.as_col();
-    arma::cx_vec a = 1.0 + 4.0 * r + 1i*dt*v/2.0;
+    int N = V.n_elem;
+    int M = V.n_cols + 2;
+
+    arma::cx_double a0 = 1.0 + 4.0 * r;
+    arma::cx_vec a = arma::cx_vec(N, arma::fill::ones)*a0;
+
+    arma::sp_cx_mat::const_iterator it = V.begin();
+    arma::sp_cx_mat::const_iterator it_end = V.end();
+
+    int i;
+    int j;
+    int k;
+
+    for(; it != it_end; ++it)
+    {
+        i = it.row();
+        j = it.col();
+        k = vec_ind(i, j, N+2);
+        
+        a(k) += 1i*dt* (*it) /2.0;
+    }
 
     return a;
 }
 
 // generates vector containing diagonal elements of matrix B
-arma::cx_vec Double_slit::a_vec(const arma::cx_mat& V, arma::cx_double r, arma::cx_double dt)
+arma::cx_vec Double_slit::b_vec(const arma::sp_cx_mat& V, arma::cx_double r, arma::cx_double dt)
 {
-    arma::cx_vec v = V.as_col();
-    arma::cx_vec a = 1.0 - 4.0 * r - 1i*dt*v/2.0;
+    int N = V.n_elem;
+    int M = V.n_cols + 2;
+
+    arma::cx_double a0 = 1.0 - 4.0 * r;
+    arma::cx_vec a = arma::cx_vec(N, arma::fill::ones)*a0;
+
+    arma::sp_cx_mat::const_iterator it = V.begin();
+    arma::sp_cx_mat::const_iterator it_end = V.end();
+
+    int i;
+    int j;
+    int k;
+
+    for(; it != it_end; ++it)
+    {
+        i = it.row();
+        j = it.col();
+        k = vec_ind(i, j, N+2);
+        
+        a(k) -= 1i*dt* (*it) /2.0;
+    }
 
     return a;
 }
 
 // Helper function for CN_matA and CN_matB
-arma::cx_mat Double_slit::tridiag(arma::cx_double r, const arma::cx_vec& di)
+arma::sp_cx_mat Double_slit::tridiag(arma::cx_double r, const arma::cx_vec& di)
 {
     int N = di.n_elem;
-    arma::cx_mat Mi = arma::diagmat(di);
 
-    // Filling the first row
-    Mi(0,0) = di(0);
-    Mi(0,1) = r;
+    // Creating diagonal matrix filled with the vector di
+    arma::sp_cx_mat Mi(N, N);
+    Mi.diag(0) = di;
 
-    // Loop that fills rows 2 to n-1 (row indices 1 to n-2)
-    for (int i = 1; i <= N-2; i++)
-    {
-        Mi(i, i-1) = r;
-        Mi(i, i) = di(i);
-        Mi(i, i+1) = r;
-    }
+    // Vector for filling the sub- and superdiagonal
+    arma::cx_vec off_diag(N - 1, arma::fill::value(r));
 
-    // Fill last row (row index n-1)
-    Mi(N-1, N-2) = r;
-    Mi(N-1, N-1) = di(N-1);
+    // Subdiagonal
+    Mi.diag(-1) = off_diag;
+
+    // Superdiagonal
+    Mi.diag(1) = off_diag;
+  
   return Mi;
 }
 
-// // Constructs matrix A for CN scheme (OPPGAVE 2)
-// arma::cx_mat Double_slit::CN_matA(int M, arma::cx_double r, arma::cx_vec a)
-// {
-//     1;
-// }
+// Constructs matrices A and B for CN scheme (OPPGAVE 2)
+arma::sp_cx_mat Double_slit::CN_matAB(arma::cx_double r, arma::cx_vec d)
+{
+    // Creating matrix A/B and filling the diagonal
+    int N = d.n_elem;
+    arma::sp_cx_mat Mat(N, N);
+    Mat.diag(0) = d;
 
-// // Constructs matrix B for CN scheme (OPPGAVE 2)
-// arma::cx_mat Double_slit::CN_matB(int M, arma::cx_double r, arma::cx_vec b)
-// {
-//     1;
-// }
+    // Vector for filling the sub- and superdiagonal
+    arma::cx_vec off_diag(N - 1, arma::fill::value(r));
+    arma::uvec indices_to_zero = arma::regspace<arma::uvec>(M - 3, M - 2, N - 2);
+    off_diag.elem(indices_to_zero).zeros();
 
+    // Subdiagonal
+    Mat.diag(-1) = off_diag;
+
+    // Superdiagonal
+    Mat.diag(1) = off_diag;
+
+    // Filling the last diagonals
+    arma::cx_vec other_diag(N - (M - 2), arma::fill::value(r));
+    Mat.diag(M - 2) = other_diag;
+    Mat.diag(-(M - 2)) = other_diag;
+
+    return Mat;
+}
+
+#include <armadillo>
+#include <vector>
+#include <string>
+
+// A function that prints the structure of a sparse matrix to screen.
+void print_sp_matrix_structure(const arma::sp_cx_mat& A)
+{
+    using namespace std;
+    using namespace arma;
+
+    // Declare a C-style 2D array of strings.
+    string S[A.n_rows][A.n_cols];  
+
+    // Initialise all the strings to " ".
+    for (int i =0; i < A.n_rows; i++)
+    {
+        for (int j = 0; j < A.n_cols; j++)
+        {
+            S[i][j] = " ";
+        }
+    }
+
+    // Next, we want to set the string to a dot at each non-zero element.
+    // To do this we use the special loop iterator from the sp_cx_mat class
+    // to help us loop over only the non-zero matrix elements.
+    sp_cx_mat::const_iterator it     = A.begin();
+    sp_cx_mat::const_iterator it_end = A.end();
+
+    int nnz = 0;
+    for(it; it != it_end; ++it)
+    {
+        S[it.row()][it.col()] = "•";
+        nnz++;
+    }
+
+    // Finally, print the matrix to screen.
+    cout << endl;
+    for (int i =0; i < A.n_rows; i++)
+    {
+        cout << "| ";
+        for (int j = 0; j < A.n_cols; j++)
+        {
+            cout << S[i][j] << " ";
+        }
+        cout <<  "|\n";
+    }
+
+    cout << endl;
+    cout << "matrix size: " << A.n_rows << "x" << A.n_cols << endl;
+    cout << "non-zero elements: " << nnz << endl ;
+    cout << endl;
+}
 
 
 
