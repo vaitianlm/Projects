67756edda09aeea27a29e503605472779f3f1ea9
root <root@DESKTOP-QK42V7B>
2024-12-08 15:25:54 +0100

    Made more plotting files. Small aditions to main and Quantum_box. Added some comments.

---
+++ python/detector.py
@@ -0,0 +1,38 @@
+import pyarma as pa
+import numpy as np
+import matplotlib.pyplot as plt
+
+# FOR PROBLEM 9
+
+# Load data from simulation
+S_arma = pa.cx_cube()
+S_arma.load("files/no_slits.bin")
+
+# Converting from arma cube to numpy array
+S = np.array(S_arma)
+P = np.abs(S)**2        # Wavefunction -> probability function
+
+
+dt = 2.5e-5                 # Simulation time step for picking out slices
+t_ind = int(0.002/dt)-1     # Slice indice at t = 0.002
+dx = 0.005                  # Simulation stepsize in space for picking out x = 0.8
+x_ind = int(0.8/dx)-1       # x-indice of x = 0.8
+
+# P(y|x=0.8; t=0.002)
+P_screen = P[t_ind, :, x_ind]/np.linalg.norm(P[t_ind, :, x_ind])
+y = np.linspace(0, 1, np.size(P_screen))
+
+# Figure to make sure it's done correctly
+vis = P
+vis[t_ind, :, x_ind] = 0.001
+plt.figure(figsize=(8, 6))
+im = plt.imshow(vis[t_ind,:,:], extent=[0, 1, 0, 1], cmap='viridis', origin='lower', aspect='auto')
+plt.colorbar(im, label='Probability') 
+plt.xlabel('x')
+plt.ylabel('y')
+plt.tight_layout()
+plt.show()
+
+# Plotting
+plt.plot(y, P_screen)
+plt.show()
--- python/plot_deviation.py
+++ python/plot_deviation.py
@@ -2,31 +2,34 @@ import pyarma as pa
 import numpy as np
 import matplotlib.pyplot as plt
 
-S = pa.cx_cube()
-S.load("files/double_slit_test.bin")
-
 norm_dev = pa.mat()
-norm_dev.load("files/deviation.bin")
+norm_dev.load("files/deviation_0slit.bin")
 
 t = np.arange(0, np.size(norm_dev))
 
-# plt.plot(t, norm_dev)
-# plt.show()
+plt.rcParams.update({"font.size": 16})
+plt.subplots_adjust( bottom = 0.138)
+plt.grid()
+plt.plot(t, norm_dev)
 
-print(pa.size(S)) #199x199x2
-
-S_py = np.array(S)
-S_py_magn = np.abs(S_py)
-
-print(np.shape(S_py))
-
-
-# Plotting
-plt.figure(figsize=(8, 6))
-im = plt.imshow(S_py_magn[0,:,:]**2, cmap='viridis', origin='lower', aspect='auto')
-plt.colorbar(im, label='Magnitude')  # Change label based on what you plot
-plt.title('First Slice of Quantum Box - Magnitude')
-plt.xlabel('X-axis (Columns)')
-plt.ylabel('Y-axis (Rows)')
-plt.tight_layout()
+plt.xlabel("Time steps")
+plt.ylabel("Deviation from 1")
 plt.show()
+
+# print(pa.size(S)) #199x199x2
+
+# S_py = np.array(S)
+# S_py_magn = np.abs(S_py)
+
+# print(np.shape(S_py))
+
+
+# # Plotting
+# plt.figure(figsize=(8, 6))
+# im = plt.imshow(S_py_magn[0,:,:]**2, cmap='viridis', origin='lower', aspect='auto')
+# plt.colorbar(im, label='Magnitude')  # Change label based on what you plot
+# plt.title('First Slice of Quantum Box - Magnitude')
+# plt.xlabel('X-axis (Columns)')
+# plt.ylabel('Y-axis (Rows)')
+# plt.tight_layout()
+# plt.show()
